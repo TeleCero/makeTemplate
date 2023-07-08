c78e37fa44f75e069897ec6e191ce49f0587c027
naielv <109038805+naielv@users.noreply.github.com>
2023-07-08 16:20:45 +0200

    Create download.py

---
+++ download.py
@@ -0,0 +1,16 @@
+import os
+
+def dfile: str, args: str = ""):
+  with open(file, "r") as f:
+    for line in f.read().splitlines():
+      try:
+        os.system(f"yt-dlp {args} {line}")
+      except Exception as e:
+        print(e)
+def dyear(year: int, args: str = ""):
+  y = str(year)
+  file = f"./links/{year}.txt"
+  dfile(file, args)
+if __name__ == "__main__":
+  dyear(2023)
+  print("makeSalvame Done")
