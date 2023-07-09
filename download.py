0068e628c01921f893c94440e7beeb0778768ea5
naielv <109038805+naielv@users.noreply.github.com>
2023-07-09 10:20:13 +0200

    Actualizar download.py

--- download.py
+++ download.py
@@ -1,16 +1,15 @@
-import os
-
-def dfile(file: str, args: str = "", out: str = "./videos/"):
-  with open(file, "r") as f:
-    for line in f.read().splitlines():
-      try:
-        os.system(f"yt-dlp {args} {line}")
-      except Exception as e:
-        print(e)
-def dyear(year: int, args: str = "", out: str = "./videos/"):
-  y = str(year)
-  file = f"./playlist/{year}.txt"
-  dfile(file, args)
-if __name__ == "__main__":
+import os 
+  
+def dfile(file: str, args: str = "", out: str = "./videos/"): 
+  with open(file, "r") as f: 
+    for line in f.read().splitlines(): 
+      try: 
+        os.system(f"yt-dlp {args} {line}") 
+      except Exception as e: 
+        print(e) 
+def dyear(year: int, args: str = "", out: str = "./videos/"): 
+  y = str(year) 
+  file = f"./playlist/{year}.txt" 
+  dfile(file, args) 
+if __name__ == "__main__": 
   dyear(2023)
-  print("makeSalvame Done")
